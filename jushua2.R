4ac4cf94ab772bf1c7b40091b708704fb3e0080f
Jushua-Espadon <jushua.espadon@students.isatu.edu.ph>
2023-09-20 08:55:45 +0000

    commit

---
+++ jushua2.R
@@ -0,0 +1,58 @@
+#1 . set up vector named age.
+numberVector <- c(34, 28, 22, 36, 27, 18, 52, 39, 42, 29,
+                  35, 31, 27, 22, 37, 34, 19, 20, 57, 49, 
+                  50, 37, 46, 25, 17, 37, 42, 53, 41,51, 35, 24, 33, 41.)
+sort(numberVector)               
+
+#a.there are 34 data points
+#b. Write the R code and its output
+seq(numberVector)
+
+assign(numberVector)
+
+min(numberVector)
+
+max(numberVector)
+
+c(numberVector)
+
+sort(numberVector)
+
+sum(numberVector)
+
+#2Find the reciprocal of the values for age.Write the R code and its output.
+recip_age<-1/numberVector
+recip_age
+
+#3 Assign also new_age <- c(age, 0, age).What happen to the new_age?
+new_age<- c(age, 0, age).
+
+#4Sort the values for age.Write the R code and its output.
+sort(numberVector)
+sort(recip_age)
+
+#5Find the minimum and maximum value for age.Write the R code and its output.
+
+
+
+#7Generates a new vector for data where you double every value of the data. | What happento the data?
+dob_data <-2*numberData
+dob_data
+
+#8.sequence
+#int(1-100)
+seq(1,100)
+#numbers(20-60)
+seq(20,60,5)
+#mean numbers (20-60)
+mean (20,60)
+#sum of (51-91)
+seq(51,91)
+sum(seq)
+#int(1-1000)
+seq(1,1000)
+
+#a.data points (8.1-8.4)
+length(seq)
+#b.R codes
+#c.max data points
